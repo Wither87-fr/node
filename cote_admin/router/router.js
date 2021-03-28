f48fe4ac72908011d9745c8ed6b4dca2fc385824
Nathan.Verdeyme <nathan.verdeyme@etu.unilim.fr>
2021-03-28 17:16:15 +0200

    cote admin à faire

---
+++ cote_admin/router/router.js
@@ -0,0 +1,30 @@
+let HomeController = require('./../controllers/HomeController');
+let VipController = require('./../controllers/VipController');
+let AlbumController = require('./../controllers/AlbumController');
+let ArticleController = require("../controllers/ArticleController");
+
+
+
+// Routes
+module.exports = function(app){
+
+// Main Routes
+    app.get('/', HomeController.Index);
+    app.get('/accueil', HomeController.Index);
+
+// VIP
+    app.get('/repertoire', VipController.Repertoire);
+    app.get('/repertoire/:letter', VipController.Search);
+    app.get('/repertoire/:letter/:num', VipController.Show);
+// albums
+    app.get('/album', AlbumController.ListerAlbum);
+    app.get('/album/:numStar', AlbumController.MontrerAlbum);
+//article
+    app.get('/articles', ArticleController.articlesAll);
+    app.get('/articles/:num', ArticleController.articlesVip);
+
+// tout le reste
+    app.get('*', HomeController.NotFound);
+    app.post('*', HomeController.NotFound);
+
+};
